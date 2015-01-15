760f4c05320e76c663b8b98e0269524d2edf2f07
amalthea5 <erin.knapp@gmail.com>
2015-01-15 18:34:09 -0500

    Able to add items to list

--- js/application.js
+++ js/application.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
+	$('#item').keyup(function(event) {
+		if(event.keyCode == 13) {
+			event.preventDefault();
+			$('#add').click();
+		};
+	});
 
-
+$('#add').click(function() {
+	event.preventDefault();
+		$('.list').append('<li class="active">' + 
+		$('input#item').val() + '</li>');
+		$('input#item').val("");
+})
 
 	
 });
