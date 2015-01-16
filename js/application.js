c8a639654062448bfabfe687c09656d9dee5ed42
amalthea5 <erin.knapp@gmail.com>
2015-01-15 19:44:34 -0500

    li are removable

--- js/application.js
+++ js/application.js
@@ -6,12 +6,25 @@ $(document).ready(function() {
 		};
 	});
 
+//add li to shopping list
 $('#add').click(function() {
 	event.preventDefault();
-		$('.list').append('<li class="active">' + 
+		$('.list').append('<li class="active"><button class="xbutton">X</button>' + 
 		$('input#item').val() + '</li>');
 		$('input#item').val("");
-})
+});
 
+//remove li by clicking on them
+$('ul').on('click', '.check', function() {
+	$(this).remove();
+});
+$('ul').on('click', '.active', function() {
+	$(this).remove();
+});
+
+//toggle class from active to check
+$('.xbutton').on('click', function() {
+	$('.active').toggleClass('check');
+});
 	
 });
